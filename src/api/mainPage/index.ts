c1fc137635fa71e69e5d1e1c85254ed7f5d01e0d
Slava Krasnianskyi <slavakrasnianskyi@Slavas-MacBook-Pro.local>
2022-11-21 15:42:19 +0200

    Middleware commit

--- src/api/mainPage/index.ts
+++ src/api/mainPage/index.ts
@@ -8,9 +8,7 @@ export const MainPageAPI = {
     url: "/content_types/main_page_hero_banner/entries/blt3085e48965238c9f?environment=dev_env&include_fallback=true",
   }),
   getModels: (): AxiosRequestConfig => ({
-    // url: "/content_types/cars_carousel_section/entries/bltc249a3d3d10085fa?environment=dev_env&include_fallback=true&include[]=tabs.tab.tab_item.tab_item.data&include[]=tabs.tab.tab_item.tab_item.data.sub_items.sub_items.items"
-    url: "/content_types/cars_carousel_section/entries/bltc249a3d3d10085fa?environment=dev_env&include_fallback=true&include[]=tabs.tab.tab_item.tab_item.data&include[]=custom.tabs.items.sub_items",
-    // url: "/content_types/cars_carousel_section/entries/bltc249a3d3d10085fa?environment=dev_env&include[]=custom.tabs.items.sub_items"
+    url: "/content_types/cars_carousel_section/entries/bltc249a3d3d10085fa?environment=dev_env&include_fallback=true&include[]=tabs.slides.slide_data",
   }),
   getCTA: (): AxiosRequestConfig => ({
     url: "/favorites"
