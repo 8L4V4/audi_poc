04c92e5b475ca923d1e03f8cac34197ad3cdc9fa
VolodymyrBuzinov <64635201+VolodymyrBuzinov@users.noreply.github.com>
2022-11-26 09:33:14 +0200

    update header styles and links

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,77 +1,26 @@
-import React, { useEffect, useState } from "react";
-import { NextPage } from "next";
-import Image from "next/dist/client/image";
-import { Icon } from "../../components/Icon/Icon";
-import { MainPageAPI } from "../../api";
-import { useApi } from "../../hooks/useApi";
-
-interface iNavData {
-  href: string;
-  html: string;
-  title: string;
-  image: string;
-}
-
-const renderNavMenuItem = (item: iNavData, i: number, arr: Array<iNavData>) => {
-  if (item.html) {
-    return (
-      <a
-        key={i}
-        href={item.href}
-        className={`Header-nav-item ${i === arr.length - 1 ? "last" : ""}`}
-        dangerouslySetInnerHTML={{ __html: item.html }}
-      />
-    );
-  }
-  return (
-    <a
-      key={i}
-      href={item.href}
-      className={`Header-nav-item ${
-        item.title === "my Audi login" ? "Header-profile" : ""
-      } ${i === arr.length - 1 ? "last" : ""}`}
-    >
-      {item.image && <Image src={item.image} alt="image" />}
-      {item.title === "my Audi login" && (
-        <Icon name="profile" className="Header-profile-icon" />
-      )}
-      {item.image && item.title ? <span>{item.title}</span> : item.title}
-    </a>
-  );
-};
+import Link from "next/link";
+import Image from "next/image";
+import logo from "components/Icon/logo.svg";
+import { ProfileIcon } from "components/Icon/ProfileIcon";
 
 export function Header() {
-  const [navData, setNavData] = useState<Array<iNavData>>([]);
-  const { apiCall } = useApi();
-
-  useEffect(() => {
-    apiCall(MainPageAPI.getNav())
-      .then(({ data }) => {
-        const resArr: iNavData[] = [];
-        data.entry.modular_blocks.forEach(
-          ({
-            menu_item: {
-              html,
-              image,
-              link: { href, title },
-            },
-          }: {
-            [k: string]: any;
-          }) => resArr.push({ html, image, href, title })
-        );
-        setNavData(resArr);
-      })
-      .catch(() => {
-        console.log(
-          "%c Error getting hheader nav data",
-          "color: red; font-size: 16px; font-weight: bold; border-left: 5px solid red"
-        );
-      });
-  }, []);
-
   return (
     <header className="Header">
-      <nav className="Header-nav"> {navData.map(renderNavMenuItem)} </nav>
+      <Link href="/" className="Header-logo">
+        <Image src={logo} alt="audi logo" />
+      </Link>
+      <nav className="Header-nav">
+        <Link className="Header-nav-item" href="/electric">
+          Audi Electric
+        </Link>
+        <Link className="Header-nav-item" href="/special-offers">
+          Special Offers
+        </Link>
+      </nav>
+      <button className="Header-profile">
+        <ProfileIcon />
+        <span>myAudi login</span>
+      </button>
     </header>
   );
 }
