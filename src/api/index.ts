c981e2d20fd339fc286a8e215f2c2ddd9e16b529
Sora <vlad@majestykapps.com>
2022-12-07 20:54:04 +0200

    fix api, fix styling electric, add filters to all models

--- src/api/index.ts
+++ src/api/index.ts
@@ -42,13 +42,13 @@ export const MainPageAPI = {
 
 export const SpecialsPageAPI = {
   getHeaderData: (): AxiosRequestConfig => ({
-    url: "content_types/specialsbanner/entries/bltb04aa584ebc391b3?environment=dev_env&include_fallback=true",
+    url: "content_types/specialsbanner/entries/bltb04aa584ebc391b3?environment=main&include_fallback=true",
   }),
   getDropDownData: (): AxiosRequestConfig => ({
-    url: "content_types/specialsdropdown/entries/blt588e2fb1dddd1c26?environment=dev_env&include_fallback=true",
+    url: "content_types/specialsdropdown/entries/blt588e2fb1dddd1c26?environment=main&include_fallback=true",
   }),
   getPresentationData: (): AxiosRequestConfig => ({
-    url: "content_types/specialspresentation/entries/bltc3f1f6e66c660120?environment=dev_env&include_fallback=true",
+    url: "content_types/specialspresentation/entries/bltc3f1f6e66c660120?environment=main&include_fallback=true",
   }),
 };
 
@@ -60,52 +60,52 @@ export const ModelsPageAPI = {
 
 export const SportModelsPageAPI = {
   getBannerData: (): AxiosRequestConfig => ({
-    url: "content_types/sportmodelsbanner/entries/blt244f0cbcfaad4e33?environment=dev_env&include_fallback=true",
+    url: "content_types/sportmodelsbanner/entries/blt244f0cbcfaad4e33?environment=main&include_fallback=true",
   }),
   getGalleyData: (): AxiosRequestConfig => ({
-    url: "content_types/sportmodelgallery/entries/blt4c8e16a43a3dcfba?environment=dev_env&include_fallback=true",
+    url: "content_types/sportmodelgallery/entries/blt4c8e16a43a3dcfba?environment=main&include_fallback=true",
   }),
 };
 
 export const InsideAudiPageAPI = {
   getBannerData: (): AxiosRequestConfig => ({
-    url: "content_types/inside_audi_banner/entries/blt2e043f18afdc0b27?environment=dev_env&include_fallback=true",
+    url: "content_types/inside_audi_banner/entries/blt2e043f18afdc0b27?environment=main&include_fallback=true",
   }),
   getGalleyData: (): AxiosRequestConfig => ({
-    url: "content_types/insideaudigallery/entries/bltaa5080b0ed9097a1?environment=dev_env&include_fallback=true",
+    url: "content_types/insideaudigallery/entries/bltaa5080b0ed9097a1?environment=main&include_fallback=true",
   }),
 };
 
 export const SportDNAPageAPI = {
   getBannerData: (): AxiosRequestConfig => ({
-    url: "content_types/sportdnabanner/entries/blt9d3d2c8b5ab5e8fd?environment=dev_env&include_fallback=true",
+    url: "content_types/sportdnabanner/entries/blt9d3d2c8b5ab5e8fd?environment=main&include_fallback=true",
   }),
   getGalleryData: (): AxiosRequestConfig => ({
-    url: "content_types/sportdnagallery/entries/blta26750b605a47426?environment=dev_env&include_fallback=true",
+    url: "content_types/sportdnagallery/entries/blta26750b605a47426?environment=main&include_fallback=true",
   }),
 };
 
 export const MotorSportHistoryPageAPI = {
   getBannerData: (): AxiosRequestConfig => ({
-    url: "content_types/sporthistorybanner/entries/blt3585540b44aae369?environment=dev_env&include_fallback=true",
+    url: "content_types/sporthistorybanner/entries/blt3585540b44aae369?environment=main&include_fallback=true",
   }),
   getArticlesData: (): AxiosRequestConfig => ({
-    url: "content_types/sporthistoryarticles/entries/blt4f167066528d8150?environment=dev_env&include_fallback=true",
+    url: "content_types/sporthistoryarticles/entries/blt4f167066528d8150?environment=main&include_fallback=true",
   }),
   getLegendsData: (): AxiosRequestConfig => ({
-    url: "content_types/sporthistorylegends/entries/bltbb364e1b1a1dbfc9?environment=dev_env&include_fallback=true",
+    url: "content_types/sporthistorylegends/entries/bltbb364e1b1a1dbfc9?environment=main&include_fallback=true",
   }),
   getSliderData: (): AxiosRequestConfig => ({
-    url: "content_types/sporthistoryslider/entries/blt34c48bb57bb4e398?environment=dev_env&include_fallback=true",
+    url: "content_types/sporthistoryslider/entries/blt34c48bb57bb4e398?environment=main&include_fallback=true",
   }),
 };
 
 export const GallerySportPageAPI = {
   getBannerData: (): AxiosRequestConfig => ({
-    url: "content_types/gallerybanner/entries/bltc9ecf9f7c3f479cb?environment=dev_env&include_fallback=true",
+    url: "content_types/gallerybanner/entries/bltc9ecf9f7c3f479cb?environment=main&include_fallback=true",
   }),
   getGalleryData: (): AxiosRequestConfig => ({
-    url: "content_types/gallery/entries/blt997c867eb81e61b4?environment=dev_env&include_fallback=true",
+    url: "content_types/gallery/entries/blt997c867eb81e61b4?environment=main&include_fallback=true",
   }),
 }
 
--- src/components/Models/ModelsList/ModelsList.tsx
+++ src/components/Models/ModelsList/ModelsList.tsx
@@ -1,30 +1,39 @@
 import { ModelsPageAPI } from "api";
 import { Loader } from "components/Loader/Loader";
-import { useApi } from "hooks/useApi";
+import { eventBus } from "helpers/EventBus/EventBus";
 import { useHttp } from "hooks/useHttp";
-import { FC, useEffect, useState } from "react";
+import { FC, ReactNode, useEffect } from "react";
 import { ModelsCard } from "../ModelsCard/ModelsCard";
 
 interface iCategories {
   headline: {url: string};
   headlineimg: {alt_img_md: {url: string;}}[];
-  list: {
-    img: {
-      img_sm: {url: string};
-      img_md: {url: string};
-      img_lg: {url: string};
-    }[];
-    price: number;
-    bold_title: string;
-    title: string;
-    url: string;
-  }[];
-}
+  list: iListItem[];
+};
 
-export const ModelsList: FC = () => {
+interface iListItem {
+  img: {
+    img_sm: {url: string};
+    img_md: {url: string};
+    img_lg: {url: string};
+  }[];
+  price: number;
+  bold_title: string;
+  title: string;
+  url: string;
+};
+
+interface iModelsList {
+  filters: {
+    active: string[] | ReactNode[] | [];
+    range: string;
+  }
+};
+
+export const ModelsList: FC<iModelsList> = ({filters}) => {
   const { call, data, isError, isLoading } = useHttp();
   const categories = data?.data?.entry?.category as iCategories[];
-  
+
   useEffect(() => {
     call(ModelsPageAPI.getCarsList())
       .catch(() => {
@@ -34,34 +43,83 @@ export const ModelsList: FC = () => {
         );
       });
   }, []);
-  
+
+  const onCalcCount = (filtredList: iCategories[]) => {
+    let result = 0;
+
+    filtredList.forEach(item => {
+      if(!!item.list.length) {
+        result += item.list.length
+      };
+    });
+    
+    return {count : result}
+  };
+
+  const filterCategory = () => {
+    if(!filters.active.length && !filters.range) return categories;
+
+    const onListFilter = (category: iCategories) => {
+      const newList: iListItem[] = [];
+    
+      const filterString = (filter: string | ReactNode) => typeof filter === "string" ? filter : "e-tron";
+      const priceFilter = Number(filters.range.replace(",", ""));
+
+      const filtredCategory = !!filters.range  ? category.list.filter(listItem => listItem.price >= priceFilter) : category.list;
+      
+      filtredCategory.map(listItem => {
+        filters.active.forEach(filter => {
+          if(listItem.title.includes(filterString(filter)) || listItem.bold_title.includes(filterString(filter))){
+            newList.push(listItem);
+          };
+          return;
+        });
+      });
+      
+      return {
+        headline: category.headline,
+        headlineimg: category.headlineimg,
+        list: newList
+      }
+    };
+
+    const result = categories.map(onListFilter)
+
+    eventBus.dispatch("toggleNotificationSidebar", onCalcCount(result));
+    return result;
+  };
+
   if(!categories || isError) return null;
   
   return (
     <div className="ModelsList">
       {isLoading && <Loader/>}
       
-      {!isLoading && categories.map(category => (
-        <div className="ModelsList-category" key={category?.headline?.url}>
-          <div className="ModelsList-category-model">
-            <img src={category?.headline?.url} alt="headline" className="ModelsList-category-model-headline"/>
-            <img src={category.headlineimg[0]?.alt_img_md?.url} alt="category_img" className="ModelsList-category-model-headlineimg" />
+      {!isLoading && filterCategory().map(category => {
+        if(!category.list.length) return;
+        
+        return (
+          <div className="ModelsList-category" key={category?.headline?.url}>
+            <div className="ModelsList-category-model">
+              <img src={category?.headline?.url} alt="headline" className="ModelsList-category-model-headline"/>
+              <img src={category.headlineimg[0]?.alt_img_md?.url} alt="category_img" className="ModelsList-category-model-headlineimg" />
+            </div>
+  
+            <div className="ModelsList-category-list">
+              {category?.list.map(card => (
+                <ModelsCard 
+                  key={Math.random() * 1000}
+                  title={card?.title}
+                  bold_title={card?.bold_title}
+                  price={card?.price}
+                  url={card?.url}
+                  img={card.img[0]?.img_md?.url}
+                />
+              ))}
+            </div>
           </div>
-
-          <div className="ModelsList-category-list">
-            {category.list.map(card => (
-              <ModelsCard 
-                key={Math.random() * 1000}
-                title={card.title}
-                bold_title={card.bold_title}
-                price={card.price}
-                url={card.url}
-                img={card.img[0].img_md.url}
-              />
-            ))}
-          </div>
-        </div>
-      ))}
+        )
+      })}
 
     </div>
   );
